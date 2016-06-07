e54cc853894a93aa8cba7f211a02cb5e3be61da2
nheyn <nick@heyn.io>
2016-06-06 20:48:19 -0600

    Get 'dispatch(...)' Tests Working
    Get the test for the 'dispatch(...)' function passing.

--- __tests__/dispatch-test.js
+++ __tests__/dispatch-test.js
@@ -1,16 +1,17 @@
-jest.unmock('../src/Store');
+jest.unmock('../src/dispatch');
 
+import Immutable from 'immutable';
 import dispatch from '../src/dispatch';
 
-xdescribe('dispatch(...)', () => {
+describe('dispatch(...)', () => {
   pit('will call each updater, with the given action', () => {
     // Test Data
     const action = { type: 'TEST_ACTION' };
-    const updaters = [
+    const updaters = Immutable.List([
       jest.fn(),
       jest.fn(),
       jest.fn()
-    ];
+    ]);
 
     // Perform Tests
     return dispatch({}, action, updaters).then(() => {
@@ -18,26 +19,26 @@ xdescribe('dispatch(...)', () => {
         const { calls } = updater.mock;
 
         expect(calls.length).toBe(1);
-        expect(calls[1]).toBe(action);
-      })
+        expect(calls[0][1]).toBe(action);
+      });
     });
   });
 
   pit('will call the first updaters, with given the state', () => {
     // Test Data
     const initialState = { data: 'test state' };
-    const updaters = [
+    const updaters = Immutable.List([
       jest.fn(),
       jest.fn(),
       jest.fn()
-    ];
+    ]);
 
     // Perform Tests
     return dispatch(initialState, { type: 'TEST_ACTION' }, updaters).then(() => {
-      const { calls } = updaters[0].mock;
+      const { calls } = updaters.get(0).mock;
 
       expect(calls.length).toBe(1);
-      expect(calls[0]).toBe(initialState);
+      expect(calls[0][0]).toBe(initialState);
     });
   });
 
@@ -48,11 +49,11 @@ xdescribe('dispatch(...)', () => {
       { data: 'updated state 1' },
       { data: 'updated state 2' }
     ];
-    const updaters = [
+    const updaters = Immutable.List([
       jest.fn().mockReturnValue(updatedStates[1]),
-      jest.fn().mockReturnValue(Promise.resolove(updatedStates[2])),
+      jest.fn().mockReturnValue(Promise.resolve(updatedStates[2])),
       jest.fn()
-    ];
+    ]);
 
     // Perform Tests
     return dispatch({}, { type: 'TEST_ACTION' }, updaters).then(() => {
@@ -63,7 +64,7 @@ xdescribe('dispatch(...)', () => {
         const { calls } = updater.mock;
 
         expect(calls.length).toBe(1);
-        expect(calls[0]).toBe(updatedStates[index]);
+        expect(calls[0][0]).toBe(updatedStates[index]);
       });
     });
   });
@@ -71,11 +72,11 @@ xdescribe('dispatch(...)', () => {
   pit('will return a promise with the state from the final updater', () => {
     // Test Data
     const finalState = { data: 'test state' };
-    const updaters = [
+    const updaters = Immutable.List([
       jest.fn(),
       jest.fn(),
       jest.fn().mockReturnValue(finalState)
-    ];
+    ]);
 
     // Perform Tests
     return dispatch({}, { type: 'TEST_ACTION' }, updaters).then((newState) => {
@@ -86,19 +87,19 @@ xdescribe('dispatch(...)', () => {
   pit('will return a reject promise if an updater throws an error', () => {
     // Test Data
     const testError = new Error();
-    const updaters = [
+    const updaters = Immutable.List([
       jest.fn(),
       jest.fn(() => { throw testError; }),
       jest.fn()
-    ];
+    ]);
 
     // Perform Tests
     return dispatch({}, { type: 'TEST_ACTION' }, updaters).catch((err) => {
       expect(err).toBe(err);
 
-      expect(updaters[0]).toBeCalled();
-      expect(updaters[1]).toBeCalled();
-      expect(updaters[2]).not.toBeCalled();
+      expect(updaters.get(0)).toBeCalled();
+      expect(updaters.get(1)).toBeCalled();
+      expect(updaters.get(2)).not.toBeCalled();
     });
   });
 });
