675e85d6eea0b632255f6eeeb92fec934c7f942f
nheyn <nick@heyn.io>
2016-06-16 23:51:11 -0600

    Code Clean Up
    Reorganize the code in Store.js, including moving in 'dispatch(...)' from dispatch.js. And removes, the now unneeded, dispatch.js file.

--- src/Store.js
+++ src/Store.js
@@ -3,17 +3,12 @@
  */
 import Immutable from 'immutable';
 
-import defaultDispatch from './dispatch';
-
-type DispatchFunc = typeof defaultDispatch;
-
 /**
  * A store that uses updaters to dispatch changes to its state.
  */
 export default class Store<S> {
   _state: S;
   _updaters: Immutable.List<Updater<S>>;
-  _dispatch: DispatchFunc;
 
   /**
    * Store constuctor (use Store.createStore).
@@ -21,10 +16,9 @@ export default class Store<S> {
    * @param state     {any}                 The current state of the object
    * @param updaters  {List<Updater>}  The updaters that can mutate the Store
    */
-  constructor(state: S, updaters: Immutable.List<Updater<S>>, dispatch: DispatchFunc) {
+  constructor(state: S, updaters: Immutable.List<Updater<S>>) {
     this._state = state;
     this._updaters = updaters;
-    this._dispatch = dispatch;
   }
 
   /**
@@ -35,7 +29,7 @@ export default class Store<S> {
    * @return              {Store} The new Store
    */
   static createStore(initialState: S): Store<S> {
-    return new Store(initialState, Immutable.List(), defaultDispatch);
+    return new Store(initialState, Immutable.List());
   }
 
   /**
@@ -45,49 +39,43 @@ export default class Store<S> {
    *
    * @throws                        An error when the given updater is not a function
    *
-   * @return      {Store}           A new Store with the new updater
+   * @return        {Store}         A new Store with the new updater
    */
   register(updater: Updater<S>): Store<S> {
     if(typeof updater !== 'function') throw new Error('updaters must be functions');
 
-    // Make sure updater always returns a promise
-    const promiseUpdater = (state, action) => Promise.resolve(updater(state, action));
-
-    return new Store(this._state, this._updaters.push(promiseUpdater), this._dispatch);
+    return new Store(this._state, this._updaters.push(updater));
   }
 
   /**
    * Update the Store by calling the actions in all the updaters.
    *
+   *  //TODO, return this (for === check) if no changes are made
+   *
    * @param action    {Object}          The action to pass to each of the updaters
    * @param settings  {[Object]}        The settings for this dispatch
+   *
+   * @throws                            An error when the action is not a plain object
+   * @throws                            An error if skip is not a number or an invalid number to skip
    *
    * @return          {Promise<Store>}  A Promise with the new Store with the state after calling the updaters
    */
   dispatch(action: Action, settings: DispatchSettings<S> = {}): Promise<Store<S>> {
-    // Check args
-    if(!action || typeof action !== 'object') throw new Error('actions must be objects');
-    if(settings.skip && typeof settings.skip !== 'number') throw new Error('settings.skip must be a number');
+    if(!action || typeof action !== 'object')                   throw new Error('actions must be objects');
+    if(settings.skip && typeof settings.skip !== 'number')      throw new Error('settings.skip must be a number');
+    if(settings.skip && settings.skip < 0)                      throw new Error('settings.skip must be positive');
+    if(settings.skip && settings.skip > this._updaters.count()) throw new Error('settings.skip is too large');
 
-    const startingState = settings.replaceState !== undefined ? settings.replaceState: this._state;
-
-    // Get updaters, w/o ones that should be skipped
-    const updatersToCall = settings.skip !== undefined? this._updaters.slice(settings.skip): this._updaters;
-
-    // Don't try to modify state if there are no updaters to perform
-    if(updatersToCall.count() === 0) {
-      return Promise.resolve(new Store(startingState, this._updaters, this._dispatch));
-    }
-
-    // Reduce the state over the updaters
-    const updatedStatePromise = this._dispatch(startingState, action, updatersToCall);
+    const currState = settings.replaceState !== undefined? settings.replaceState: this._state;
+    const currUpdaters = settings.skip !== undefined? this._updaters.slice(settings.skip): this._updaters;
 
     // Create new store from the new state
-    return updatedStatePromise.then((newState) => {
+    return dispatch(currState, action, currUpdaters).then((updatedState) => {
       // Check for valid state
-      if(!this._isValidState(newState)) throw new Error('a state must be returned from each updater');
+      if(typeof updatedState === 'undefined') throw new Error('a state must be returned from each updater');
 
-      return new Store(newState, this._updaters, this._dispatch);
+      // Return 'this' if the state has not changed, else create Store with the updated state
+      return this._state === updatedState? this: new Store(updatedState, this._updaters);;
     });
   }
 
@@ -99,10 +87,6 @@ export default class Store<S> {
   getState(): S {
     return this._state;
   }
-
-  _isValidState(testState: any): boolean {
-    return typeof testState !== 'undefined';
-  }
 }
 
 /**
@@ -110,4 +94,25 @@ export default class Store<S> {
  */
 export function createStore<S>(initialState: S): Store<S> {
   return Store.createStore(initialState);
+}
+
+/**
+ * Perform a dispatch using the given updaters.
+ *
+ * @param state   {any}             The initial state to send through the updaters
+ * @param action  {Action}          The action to pass to the updaters
+ * @param updater {List<Updaters>}  The updaters to call
+ *
+ * @return        {Promise<any>}    The updated state in a Promise
+ */
+function dispatch<S>(state: S, action: Action, updaters: Immutable.List<Updater<S>>): Promise<S> {
+  // Go through each updater (wrapped in a promise)
+  return updaters.reduce((currStatePromise, updater, index) => {
+    return currStatePromise.then((state) => {
+      // Call current updater
+      const newState = updater(state, action);
+
+      return Promise.resolve(newState);
+    });
+  }, Promise.resolve(state));
 }
