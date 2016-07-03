9cccafac12b05b81a153cadc517cf814ea0b3510
nheyn <nick@heyn.io>
2016-07-03 13:23:03 -0600

    Add PausePromise Class
    Add a utility class, PausePromise, that will work as a place holder for Promises after a dispatch has been paused.

---
+++ src/utils/PausePromise.js
@@ -0,0 +1,57 @@
+/**
+ * @flow
+ */
+import Immutable from 'immutable';
+
+type PromiseFunc<I, F> = {
+  thenFunc?: (value: I) => F | Promise<F>,
+  catchFunc?: (err: Error) => F | Promise<F>
+};
+
+/**
+ * Place holder for promises used after a pause is called.
+ */
+export default class PausePromise {
+  isPausePromise: bool;
+  _promise: Promise<any>;
+  _promiseFuncs: Immutable.List<PromiseFunc<any, any>>;
+
+  constructor(promise: Promise<any>, promiseFuncs: Immutable.List<PromiseFunc<any, any>>) {
+    this.isPausePromise = true;
+
+    this._promise = promise;
+    this._promiseFuncs = promiseFuncs;
+  }
+
+  static createPausePromise(promise: Promise<any>): PausePromise {
+    return new PausePromise(promise, Immutable.List());
+  }
+
+  static isPausePromise(maybePausePromise: any): bool {
+    return maybePausePromise && maybePausePromise.isPausePromise? true: false;
+  }
+
+  getPromise(): Promise<any> {
+    return this._promise;
+  }
+
+  resolveFor(maybePromise: any | Promise<any>): Promise<any> {
+    return this._promiseFuncs.reduce((currPromise, { thenFunc, catchFunc }) => {
+      return currPromise.then(thenFunc, catchFunc);
+    }, Promise.resolve(maybePromise));
+  }
+
+  then(thenFunc: (value: any) => any, catchFunc?: (err: Error) => any): PausePromise {
+    return new PausePromise(
+      this._promise,
+      this._promiseFuncs.push({ thenFunc, catchFunc })
+    );
+  }
+
+  catch(catchFunc: (value: any) => any): PausePromise {
+    return new PausePromise(
+      this._promise,
+      this._promiseFuncs.push({ catchFunc })
+    );
+  }
+}
